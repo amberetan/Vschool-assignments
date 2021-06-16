aae57ad8c2f85fdfba18464110c15bba0f1caa09
Amber Tan <amber.e.tan@gmail.com>
2021-06-16 09:39:01 -0600

    found solution, nested 3 for loops to achieve desired result.

--- forception/index.js
+++ forception/index.js
@@ -3,12 +3,13 @@ function forception(people, alphabet){
     var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
     var alphabet = "abcdefghijklmnopqrstuvwxyz"
     var newAlphabet = alphabet.split('')
-
+    var newArr = []
     for (var i = 0; i < people.length; i++){
-        var newArr = []
         newArr.push(people[i])
-        for (var j = 0; j <people.length; j++){
-            newArr.concat(newAlphabet)
+        for (var j = 0; j < 1; j++){
+            for (var k = 0; k < newAlphabet.length; k++){
+                newArr.push(newAlphabet[k])
+            }
         }
     }
     console.log(newArr)
