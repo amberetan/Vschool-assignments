1658aa995c45d5d5ef74aeb4cd9ffade57af63f6
Amber Tan <amber.e.tan@gmail.com>
2021-06-28 14:41:32 -0600

    added random pickers for arrays, and added 'false alarms'.

--- rpgCapstone/index.js
+++ rpgCapstone/index.js
@@ -1,10 +1,6 @@
 const readlineSync = require('readline-sync');
-const userName = readline.question("Hello, weary traveler. Welcome to the Realm of Darkness. Beyond lay treasures and nightmares beyond your imagination. Give me your name if you are foolish enough to enter: ")
 
-
-
-
-
+const userName = readlineSync.question("Hello, weary traveler. Welcome to the Realm of Darkness. Beyond lay treasures and nightmares beyond your imagination. Give me your name if you are foolish enough to enter: ")
 const user = {
     userName: userName,
     isAlive: true,
@@ -13,6 +9,7 @@ const user = {
 
     }
 }
+
 
 const wildEnemy = [
     werewolf = {
@@ -64,7 +61,7 @@ const wildEnemy = [
 
 const specialItems = [
     healingPotion = {
-        addHP: 15,
+        addHP: 100,
         caption: "  The inscription reads: For use on flesh wounds.  "
     },
     questionableHerbs = {
@@ -76,15 +73,15 @@ const specialItems = [
         caption: "  The inscription reads: From Jerry. Don't ask.  "
     },
     ichneumonArmor = {
-        addStrength: 15,
+        addStrength: 50,
         caption: "  It has a rough exterior, but makes for cunning camouflaging.  "
     },
     smallDagger = {
-        addStrength: 2,
+        addStrength: 10,
         caption: "  A rusty blade, probably left behind by a less-cunning, fool-hardy traveler.  "
     },
     masterSword = {
-        addStrength = 20,
+        addStrength: 50,
         caption: "  It is well-balanced, always sharp and fills you with the righteous wind of the Goddess.  "
     },
     knightsEmerald = {
@@ -92,7 +89,7 @@ const specialItems = [
         caption: "  A glinting green behind the dirt, you feel luck and speed surge through your hand as you hold it.  "
     },
     wizardsOpal = {
-        addStrength = 25,
+        addStrength: 50,
         caption: "  Everchanging, it has a familiar feel to it, as if you've always held it.  "  
     },
     sirensRuby = {
@@ -100,3 +97,48 @@ const specialItems = [
         caption: "  An eerie feeling pricks up your neck, like all eyes in the forest are suddenly aware of you.  "
     }
 ]
+
+const dragonKing = {
+    HP: 200,
+    isAlive: true,
+}
+
+const falseAlarms = [
+    "Something flutters quickly past your ear, " ,
+    "A sudden crack of thunder nearby startles you, ",
+    "A ear-splitting shriek rings out through the air, ",
+    "A thudding of large footsteps can be heard to your right, ",
+    "A high-pitched maniacal laugh creeps through the air above you, ",
+    "The struggling cries of something small can be heard in the near distance, ",
+    "An incredibly bright flash of light streaks through the sky above, ",
+    "Something dark and furry slithers across your path ahead, ",
+    "A low and rumbling growl drifts over from your left, "
+]
+
+
+
+
+const startGame = readlineSync.question("Well, " + userName + " you have proven your bravery and determination already. There is no turning back. The only escape is death, yours or the Dragon Kings. Keep walking ('W'), attack what comes before you ('A') or try to run if you dare ('R'). You may see your health status and items you have collected by pressing 'P'. Will you walk in the woods?  ")
+
+if (startGame === "W" || startGame === "w"){
+    let isPLayerAlive = true
+    while (isPLayerAlive) {
+        console.log("You are walking in the woods...")
+        const enemyProbability = Math.floor(Math.random() * 101)
+        if(enemyProbability >= 50) {
+            const randomEnemyPicker =  Math.floor(Math.random() * 10)
+            const randomEnemy = wildEnemy[randomEnemyPicker]
+            const walking = readlineSync.question("As you walk through the woods, you hear something on the path before you. Suddenly, a" + randomEnemy.description + "appears in your path.  Will you attack or run?  ")
+        } else {
+            const randomFalseAlarmPicker = Math.floor(Math.random() * 10)
+            const randomFalseAlarm = falseAlarm[randomFalseAlarmPicker]
+            const keepWalking = readlineSync.question(randomFalseAlarm + "and then everything is quiet.  Will you continue to walk in the woods?  ")
+        }
+
+    }
+
+}
+
+
+
+
